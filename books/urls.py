51fd14127f7f941aff65b83d5f317aee699cdae8
Mohammad Tavakoli <mohammadh.tavakoli81@gmail.com>
2025-05-22 19:22:21 +0330

    Refactored views/urls with Swagger decorators

--- books/urls.py
+++ books/urls.py
@@ -10,10 +10,10 @@ from books.utils.swagger_docs import (
 
 
 urlpatterns = [
-    path('home/', books_home_schema(views.books_home), name='books-home'),
-    path('search/', search_books_schema(views.search_books), name='search-books'),
-    path('detail/<str:book_link>/', book_detail_schema(views.book_detail), name='book-detail'),
-    path('categories/', all_categories_schema(views.all_categories), name='all-categories'),
-    path('fetch-category/', fetch_category_books_schema(views.fetch_category_books), name='fetch-category-books'),
+    path('home/', views.books_home, name='books-home'),
+    path('search/', views.search_books, name='search-books'),
+    path('detail/<str:book_link>/', views.book_detail, name='book-detail'),
+    path('categories/', views.all_categories, name='all-categories'),
+    path('fetch-category/', views.fetch_category_books, name='fetch-category-books'),
 
 ]
